b76759da218dfb52c50af9d199917f57726ee000
Eduardo-Iza <edu70.i@gmail.com>
2021-10-28 12:12:34 -0300

    nuevo app.js

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-const path = require('path');
 const express = require('express');
 const server = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
-
 server.use ("/static", express.static("public"));
-server.set("views", path.resolve(__dirname, "views"));
+server.set("views",path.join(path.dirname(__dirname,"views")));
 
+server.listen(port, () =>{
+    console.log("port 3000")
+});
+server.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,"views/home.html"));
+});
 
-server.listen(port, () => {
-     console.log('Servidor iniciado en el puerto 3000')
-    });
-server.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname,"views/home.html"));
-});
-server.get('/login', (req, res) => {
-        res.sendFile(path.join(__dirname, '/views/login.html'));
-});
-server.get('/register', (req, res) => {
-        res.sendFile(path.join(__dirname, '/views/register.html'));
-})
