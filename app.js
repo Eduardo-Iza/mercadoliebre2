559f407197c0b91bd8a08084760be60aa5342c63
Eduardo-Iza <edu70.i@gmail.com>
2021-10-28 16:01:44 -0300

    correcciones en app

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const publicPath = path.resolve(__dirname, "./public");
+/*const publicPath = path.resolve(__dirname, "./public");*/
 const port = process.env.PORT || 3000;
 
-app.use ("/static", express.static(publicPath));
-app.set("views",path.join(path.dirname(__dirname,"views")));
+app.use ("/static", express.static('public'));
+/*app.set("views",path.join(path.dirname(__dirname,"views")));*/
 
 app.listen(port, () =>{
     console.log("port 3000")
 });
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'views/home.html'));
+    res.sendFile(path.join(__dirname,'/views/home.html'));
     
 });
 app.get('/login', (req,res) => {
-    res.sendFile(path.join(__dirname,'views/login.html'));
+    res.sendFile(path.join(__dirname,'/views/login.html'));
 });
 app.get('/register', (req,res) => {
-    res.sendFile(path.join(__dirname,'views/register.html'));
+    res.sendFile(path.join(__dirname,'/views/register.html'));
 });
