84d55041bf27d11c11ba329ce8a5b4ac191dfee5
Eduardo-Iza <edu70.i@gmail.com>
2021-10-28 23:44:24 -0300

    correcciones en app

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 /*const publicPath = path.resolve(__dirname, "./public");*/
 const port = process.env.PORT || 3000;
+app.listen(port, () =>{
+    console.log("port 3000")
+});
 
 app.use ("/static", express.static('public'));
 /*app.set("views",path.join(path.dirname(__dirname,"views")));*/
 
-app.listen(port, () =>{
-    console.log("port 3000")
-});
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'));
     
